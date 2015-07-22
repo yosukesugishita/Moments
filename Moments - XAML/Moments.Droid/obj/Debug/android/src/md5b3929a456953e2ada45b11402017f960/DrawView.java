aa9b955cef6877d846a432abd0e90b8d3c6ca221
Pierce Boggan <pierce@pierceboggan.com>
2015-07-22 02:30:38 -0500

    Enjoy Moments :)

---
+++ Moments - XAML/Moments.Droid/obj/Debug/android/src/md5b3929a456953e2ada45b11402017f960/DrawView.java
@@ -0,0 +1,88 @@
+package md5b3929a456953e2ada45b11402017f960;
+
+
+public class DrawView
+	extends android.view.View
+	implements
+		mono.android.IGCUserPeer
+{
+	static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onSizeChanged:(IIII)V:GetOnSizeChanged_IIIIHandler\n" +
+			"n_onDraw:(Landroid/graphics/Canvas;)V:GetOnDraw_Landroid_graphics_Canvas_Handler\n" +
+			"n_onTouchEvent:(Landroid/view/MotionEvent;)Z:GetOnTouchEvent_Landroid_view_MotionEvent_Handler\n" +
+			"";
+		mono.android.Runtime.register ("Moments.Android.DrawView, Moments.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", DrawView.class, __md_methods);
+	}
+
+
+	public DrawView (android.content.Context p0) throws java.lang.Throwable
+	{
+		super (p0);
+		if (getClass () == DrawView.class)
+			mono.android.TypeManager.Activate ("Moments.Android.DrawView, Moments.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "Android.Content.Context, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065", this, new java.lang.Object[] { p0 });
+	}
+
+
+	public DrawView (android.content.Context p0, android.util.AttributeSet p1) throws java.lang.Throwable
+	{
+		super (p0, p1);
+		if (getClass () == DrawView.class)
+			mono.android.TypeManager.Activate ("Moments.Android.DrawView, Moments.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "Android.Content.Context, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065:Android.Util.IAttributeSet, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065", this, new java.lang.Object[] { p0, p1 });
+	}
+
+
+	public DrawView (android.content.Context p0, android.util.AttributeSet p1, int p2) throws java.lang.Throwable
+	{
+		super (p0, p1, p2);
+		if (getClass () == DrawView.class)
+			mono.android.TypeManager.Activate ("Moments.Android.DrawView, Moments.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "Android.Content.Context, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065:Android.Util.IAttributeSet, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065:System.Int32, mscorlib, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e", this, new java.lang.Object[] { p0, p1, p2 });
+	}
+
+
+	public DrawView (android.content.Context p0, android.util.AttributeSet p1, int p2, int p3) throws java.lang.Throwable
+	{
+		super (p0, p1, p2, p3);
+		if (getClass () == DrawView.class)
+			mono.android.TypeManager.Activate ("Moments.Android.DrawView, Moments.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "Android.Content.Context, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065:Android.Util.IAttributeSet, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=84e04ff9cfb79065:System.Int32, mscorlib, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e:System.Int32, mscorlib, Version=2.0.5.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e", this, new java.lang.Object[] { p0, p1, p2, p3 });
+	}
+
+
+	public void onSizeChanged (int p0, int p1, int p2, int p3)
+	{
+		n_onSizeChanged (p0, p1, p2, p3);
+	}
+
+	private native void n_onSizeChanged (int p0, int p1, int p2, int p3);
+
+
+	public void onDraw (android.graphics.Canvas p0)
+	{
+		n_onDraw (p0);
+	}
+
+	private native void n_onDraw (android.graphics.Canvas p0);
+
+
+	public boolean onTouchEvent (android.view.MotionEvent p0)
+	{
+		return n_onTouchEvent (p0);
+	}
+
+	private native boolean n_onTouchEvent (android.view.MotionEvent p0);
+
+	java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
